e8d0f96914afa638f99382d4207efe495c5fb983
Oghenebrume50 <raphlbrume@gmail.com>
2019-08-14 11:27:28 +0100

    setup the header

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
   }
 };
   
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-const y = "good boy";
+function setHeader() {
+  const content = document.getElementById('content');
+  const navbar = document.createElement('nav');
+  const name = document.createElement('h1');
+  name.innerText = 'Sofiri'
+  const menuButton = document.createElement('a')
+  menuButton.innerText = 'Menu'
+  const aboutUsButton = document.createElement('a')
+  aboutUsButton.innerText = 'About us'
+  const contactUsButton = document.createElement('a')
+  contactUsButton.innerText = 'Contact us'
+  navbar.appendChild(name);
+  navbar.appendChild(menuButton);
+  navbar.appendChild(aboutUsButton);
+  navbar.appendChild(contactUsButton);
+  content.appendChild(navbar);
+}
+
+function render(){
+  window.addEventListener('DOMContentLoaded', (event) => {
+    setHeader();
+  });
+}
+
+render();
